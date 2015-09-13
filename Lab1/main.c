0d7b45c7f81183b0c21290d024de44bbffed1ff4
QSSR <peter.raspe@gmail.com>
2015-09-13 12:55:47 -0400

    Added in a PID Control Section to main
    Not certain whether to make a separate PID library or to ad the
    constants into globals

--- Lab1/main.c
+++ Lab1/main.c
@@ -7,19 +7,33 @@
 #include "RBELib/RBELib.h" //RBELib
 #include "globals.h"
 #include <avr/io.h>
+int lastError, pot;
 
 
-
-
+int PID(int error, int newError){
+	double P, I, D; 
+	int output;
+	P = 0; //Proportional 
+	I = 0; //Integral
+	D = 0; //Derivative
+	int Kp, Ki, Kd; //TODO tune the constants
+	P = Kp * error;
+	I = Ki * (I + error * time);
+	D = Kd * ((newError - error)/(time))
+	output = P + I + D;
+	return output;
+}
 
 int main(void){
+	lastError = globals.ADCval; //Does this need to be in globals?
 	initGlobals();
 	initRBELib(); //initialize RBELib
 	initADC(1);
 	debugUSARTInit(115200); // initialize USART
 	volatile int x = globals.ADCval;
 	printf("ADC Value %d \n", x);
-	return 0;
-
+	pot = PID(x, lastError); //get the PID controller
+	printf("PID Value %d \n", globals.pot)
+	return 0;	
 }
 
